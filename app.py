98d6da884b869bb6988705ff72313d2cc5ca721b
Tassy726 <a160@switchon.jp.net>
2025-09-01 17:47:45 +0900

    最小機能（表示だけ）

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # アプリ名：メモ帳アプリ
+import os
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    # ローカル開発用
+    app.run(debug=True)
